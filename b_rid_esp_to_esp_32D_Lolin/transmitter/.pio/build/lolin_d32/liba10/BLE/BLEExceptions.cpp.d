1af447f3cec86bdaee9657a6937b45645fa2fd12
Dlyse <88264614+Dlyse@users.noreply.github.com>
2026-08-06 13:17:36 +0800

    file reallocation

--- b_rid_esp_to_esp_32D_Lolin/transmitter/.pio/build/lolin_d32/liba10/BLE/BLEExceptions.cpp.d
+++
@@ -1,2 +0,0 @@
-.pio/build/lolin_d32/liba10/BLE/BLEExceptions.cpp.o: \
- C:/Users/LHY00/.platformio/packages/framework-arduinoespressif32/libraries/BLE/src/BLEExceptions.cpp
--- b_rid_esp_to_esp_32D_Lolin/transmitter/.pio/build/lolin_d32/src/opendroneid.c.d
+++
@@ -1,2 +0,0 @@
-.pio/build/lolin_d32/src/opendroneid.c.o: src/opendroneid.c \
- include/opendroneid.h
